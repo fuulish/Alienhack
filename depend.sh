c95f6e70c8ca79bdd558ad3f6fbdd5af38a488f8
Frank Uhlig <uhlig.frank@gmail.com>
2022-07-13 20:00:51 +0200

    chg: find full path of all dependencies

--- depend.sh
+++ depend.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$1"
 shift 1
-g++ -MM -MG "$@" | sed -E 's#^(.*\.o: *)src/(.*/)?(.*\.cpp)#build/\2\1src/\2\3#' | sed -E 's#^(.*\.o: *)RL-Shared/(.*/)?(.*\.cpp)#build/\2\1RL-Shared/\2\3#'
+RULE=$(g++ -MM -MG "$@" | sed -E 's#^(.*\.o:*)(.*/)?(.*\.cpp)#\2\1\2\3#')
+DEPS=
+for fname in $(echo "$RULE" | tr -d '\n'  | cut -d':' -f2 | sed s/'\\'//g); do
+        f=$(basename $fname)
+
+        for parts in $(echo $fname | sed s/'\.\.\/'/' '/g); do
+                grepme=$parts
+        done
+
+        DEPS="$DEPS $(find . -name $f | sed s/'\.\/'//g | grep $grepme)"
+done
+TGT=$(echo "$RULE" | tr -d '\n' | cut -d':' -f1 | sed s/'\s'/''/g)
+echo "$TGT: $DEPS"
